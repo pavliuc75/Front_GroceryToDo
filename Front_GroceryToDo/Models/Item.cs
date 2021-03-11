ebd8b88df6df2b3347d096051051e2a7952852b3
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-03-11 20:43:37 +0100

    added models

---
+++ Front_GroceryToDo/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Front_GroceryToDo.Models
+{
+    public class Item
+    {
+        [JsonPropertyName("id")] public int Id { get; set; }
+        [JsonPropertyName("name")] public string Name { get; set; }
+        [JsonPropertyName("quantity")] public int Quantity { get; set; }
+        [JsonPropertyName("weight")] public double Weight { get; set; }
+    }
+}
---
+++ Front_GroceryToDo/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Front_GroceryToDo.Models
+{
+    public class Record
+    {
+        [JsonPropertyName("id")] public int Id { get; set; }
+        [JsonPropertyName("items")] public List<Item> Items { get; set; }
+    }
+}
