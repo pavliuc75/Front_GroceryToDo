06e20e2e3e32857ff6a3c2a5e657c1be3607f398
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-03-16 20:03:47 +0100

    minor model update

--- Front_GroceryToDo/Models/Item.cs
+++ Front_GroceryToDo/Models/Item.cs
@@ -6,7 +6,7 @@ namespace Front_GroceryToDo.Models
     {
         [JsonPropertyName("id")] public int Id { get; set; }
         [JsonPropertyName("name")] public string Name { get; set; }
-        [JsonPropertyName("completed")] public bool Completed { get; set; }
+        [JsonPropertyName("isCompleted")] public bool IsCompleted { get; set; }
         [JsonPropertyName("quantity")] public int Quantity { get; set; }
         [JsonPropertyName("weight")] public double Weight { get; set; }
         [JsonPropertyName("unit")] public string Unit { get; set; }
